129130a93f84cf174daeee6bc2a6f78c59939605
Sammy Franklin <sam@hprb-rtr.stanford.edu>
2018-02-17 01:30:54 -0800

    added initial react setup

---
+++ react/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/home.jsx';
+import Error404 from './components/404.jsx';
+
+class Index extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/404" component={Error404}/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  console.info("justsignin");
+  ReactDOM.render(
+    <HashRouter> 
+      <Index/>
+    </HashRouter>, document.getElementById("app")
+  );
+});
